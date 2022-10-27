61a4872ca8be4d2b40d8b46e0af935460fac7e40
Enzo <96829831+bacqueyrisses@users.noreply.github.com>
2022-10-27 23:36:46 +0200

    Added randomNumberBetween function

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const capitalize = (searchQuery) =>
+const capitalize = (searchQuery) => {
   searchQuery.charAt(0).toUpperCase() + searchQuery.slice(1);
+};
 
-module.exports = { capitalize };
+const randomNumberBetween = (min, max) => {
+  return Math.random() * (max - min) + min;
+};
+
+module.exports = { capitalize, randomNumberBetween };
